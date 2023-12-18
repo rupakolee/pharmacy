9db2d4c9eb5924bdd9a6fce45ec49b7771c9e1bf
rupakolee <rupakolee@gmail.com>
2023-12-18 17:27:44 +0545

    updates

---
+++ login.js
@@ -0,0 +1,6 @@
+let userId = document.querySelector('#user-id');
+let passcode = document.querySelector('#passcode');
+let loginBtn = document.querySelector('#submit');
+
+loginBtn.addEventListener('click', () => {
+})
