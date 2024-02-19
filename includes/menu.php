6a6257a7b822c8b1da9c6fe08d76816e0c1e9407
rupakolee <rupak.edzon11@gmail.com>
2024-02-19 22:11:25 +0545

    my information added

--- includes/menu.php
+++ includes/menu.php
@@ -6,7 +6,7 @@
             <div class="user-info">
                 <button id="user-btn"><img src="../images/user.png" alt=""></button>
                 <ul class="user-panel">
-                <li><a href="../menu/profile.php">My information</a></li>
+                <li><a href="../menu/myInfo.php">My information</a></li>
                 <li><a href="../menu/settings.php">Settings</a></li>
                 <li><a href="../menu/password.php">Change Password</a></li>
                     <li>Log out<button id="logout-btn" onclick="window.location.href='../login/login.php'"><img src="../images/logout.png" alt="logout"></button></li>
---
+++ menu/myInfo.php
@@ -0,0 +1,22 @@
+<?
+$records = select($conn, 'register');
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Information</title>
+</head>
+<body>
+    <h2>My Information</h2>
+    <?php foreach($records as $record): ?>
+    <p>User Name: <?= $record['fullName']; ?></p>
+    <p>Pharmacy's Name: <?= $record['pharmacyName']; ?></p>
+    <p>Email: <?= $record['email']; ?></p>
+    <p>Phone: <?= $record['phone']; ?></p>
+    <?php endforeach; ?>
+</body>
+</html>
