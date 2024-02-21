579554404ce43e822a804b9f9e9d508c26c019aa
rupakolee <rupak.edzon11@gmail.com>
2024-02-21 21:39:07 +0545

    styling ins progresss

--- home/home.php
+++ home/home.php
@@ -40,33 +40,42 @@ if ($result == false) {
             </div>
         </nav>
 
+        <hr style="color: #fff;">
         <!-- interface -->
 
         <!-- left navigation panel -->
 
-        <div class="menu">
-            <button id="menu-btn"><img src="../images/menu.png" alt="menu" style="width: 36px;"></button>
-            <ul id="menu-list">
-                <li><a href="home.php">Dashboard</a></li>
-                <li><a href="purchase.php">Purchase</a></li>
-                <li><a href="invoice.php">Invoice</a></li>
-                <li><a href="sales.php">Sales</a></li>
-                <li><a href="inventory.php">Medicines</a></li>
-                <li><a href="customers.php">Customers</a></li>
-                <li><a href="vendor.php">Vendors</a></li>
-            </ul>
-        </div>
+        
         <div class="main">
-
+            
+            <div class="menu">
+                <button id="menu-btn"><img src="../images/menu.png" alt="menu" style="width: 36px;"></button>
+                <ul id="menu-list">
+                    <li><a href="home.php">Dashboard</a></li>
+                    <li><a href="purchase.php">Purchase</a></li>
+                    <li><a href="invoice.php">Invoice</a></li>
+                    <li><a href="sales.php">Sales</a></li>
+                    <li><a href="inventory.php">Medicines</a></li>
+                    <li><a href="customers.php">Customers</a></li>
+                    <li><a href="vendor.php">Vendors</a></li>
+                </ul>
+            </div>
             <!-- dashboard -->
 
             <div class="dashboard">
-                <h3>Total sales</h3>
-            </div>
-
-            <!-- mails and notifications -->
-            <div class="inbox">
-
+                <h2>Dashboard</h2>
+                <div class="grid">
+                    <div class="box">Total Customer</div>
+                    <div class="box">Medicines Expired</div>
+                    <div class="box">Total Invoice</div>
+                    <div class="box">No. of Vendors</div>
+                    <div class="sales-panel">Total Sales</div>
+                </div>
+                
+                <!-- mails and notifications -->
+                <div class="inbox">
+                    
+                </div>
             </div>
         </div>
 
