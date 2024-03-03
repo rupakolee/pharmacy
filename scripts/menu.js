3112bcac76b4f185648e659bc6d54c52b1a84fe7
rupakolee <rupakolee@gmail.com>
2024-03-03 12:58:35 +0545

    menu button completed

--- scripts/menu.js
+++ scripts/menu.js
@@ -6,11 +6,27 @@ const menuBox = document.querySelector('.menu-box');
 menuBox.addEventListener('click', function toggleMenu() {
     if(menuList.style.display === "none") {
         menuList.style.display = "block";
+        line1.style.transform= "translate(0, 10px) rotate(45deg)";
+        line3.style.transform= "translate(0, -10px) rotate(-45deg)";
+        line2.style.opacity = '0';
     }
     else {
         menuList.style.display = "none";
+        line1.style.transform= "translate(0, 0) rotate(0)";
+        line3.style.transform= "translate(0, 0) rotate(0)";
+        line2.style.opacity = '1';
     }
 });
+
+//changing color of menu icon
+
+let menuLines = document.querySelectorAll('.line');
+let line1 = document.querySelector('.line1');
+let line2 = document.querySelector('.line2');
+let line3 = document.querySelector('.line3');
+menuBox.addEventListener('mouseover', () => {
+    menuLines.style.backgroundColor = "white";
+})
 
 // user-panel display/hide
 
