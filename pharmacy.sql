2f8f0b6dd19cb8a97dde750e6b5051d58ae82390
rupakolee <rupakolee@gmail.com>
2024-03-24 14:23:23 +0545

    updated database

--- pharmacy.sql
+++ pharmacy.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 08, 2024 at 07:13 AM
+-- Generation Time: Mar 24, 2024 at 09:36 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -41,9 +41,7 @@ CREATE TABLE `customer` (
 --
 
 INSERT INTO `customer` (`id`, `dob`, `sex`, `name`, `address`, `contact`) VALUES
-(1, '2024-02-17', 'M', 'Rupak', 'Basundhara', '9888'),
-(2, '2002-07-10', 'M', 'Bepin', 'Lainchaur', '0099'),
-(3, '2002-07-10', 'M', 'Bepin', 'Lainchaur', '0099');
+(1, '2024-02-17', 'M', 'Rupak', 'Basundhara', '9888');
 
 -- --------------------------------------------------------
 
@@ -67,11 +65,8 @@ CREATE TABLE `invoice` (
 
 INSERT INTO `invoice` (`id`, `customer_name`, `medicine_name`, `quantity`, `rate`, `total`, `date`) VALUES
 (1, 'Rupak', 'Paracetamol', 12, 100, 1200, '2024-02-16'),
-(28, 'Bipin', 'Ascoril', 5, 10, 50, '2024-02-19'),
 (33, 'Bipin', 'Ascoril-D', 10, 120, 1200, '2024-02-19'),
-(34, 'Rupak', 'Ascoril', 10, 120, 1200, '2024-02-19'),
 (35, 'Nishan', 'Petanfast', 10, 10, 100, '2024-02-19'),
-(36, 'ggh', 'Telma H', 10, 10, 100, '2024-02-29'),
 (37, 'Te', 'Telma H', 10, 10, 100, '2024-02-29');
 
 -- --------------------------------------------------------
@@ -86,6 +81,7 @@ CREATE TABLE `medicine` (
   `category` varchar(128) NOT NULL,
   `quantity` int(8) NOT NULL,
   `price` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
   `expiry` date NOT NULL,
   `date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -94,11 +90,9 @@ CREATE TABLE `medicine` (
 -- Dumping data for table `medicine`
 --
 
-INSERT INTO `medicine` (`id`, `name`, `category`, `quantity`, `price`, `expiry`, `date`) VALUES
-(25, 'Melmet SR -1000', 'Tablet', 1000, 100, '2024-03-09', '2024-02-26'),
-(26, 'Melmet SR -1000', 'Tablet', 1000, 100, '2024-03-09', '2024-02-26'),
-(27, 'Telma H', 'Tablet', 80, 10, '2024-02-27', '2024-02-26'),
-(31, 'Pentafast', 'Tablet', 10, 10, '2024-03-22', '2024-03-08');
+INSERT INTO `medicine` (`id`, `name`, `category`, `quantity`, `price`, `total`, `expiry`, `date`) VALUES
+(31, 'Pentafast', 'Tablet', 10, 10, 0, '2024-03-22', '2024-03-08'),
+(32, 'Paracetamol', 'Tablet', 10, 25, 250, '2024-03-30', '2024-03-20');
 
 -- --------------------------------------------------------
 
@@ -112,16 +106,20 @@ CREATE TABLE `register` (
   `pharmacyName` varchar(128) NOT NULL,
   `pass` varchar(128) NOT NULL,
   `email` varchar(128) NOT NULL,
-  `phone` varchar(128) NOT NULL
+  `phone` varchar(128) NOT NULL,
+  `date` date DEFAULT NULL,
+  `status` varchar(50) NOT NULL DEFAULT 'active',
+  `token` varchar(64) NOT NULL,
+  `token_expiry` datetime DEFAULT NULL,
+  `verification_code` int(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `register`
 --
 
-INSERT INTO `register` (`id`, `fullName`, `pharmacyName`, `pass`, `email`, `phone`) VALUES
-(3, 'Rupak Olee', 'Rupak\'s P', '1111', 'rupak@gmail.com', '9878987678'),
-(8, 'Shahil Hussain', 'Shahil Pharmacy (P) Ltd.', 'sahil', 'shahil@gmail.com', '9813169236');
+INSERT INTO `register` (`id`, `fullName`, `pharmacyName`, `pass`, `email`, `phone`, `date`, `status`, `token`, `token_expiry`, `verification_code`) VALUES
+(3, 'Rupak Olee', 'Rupak\'s P', '1111', 'rupak@gmail.com', '9878987678', NULL, 'active', '5bdb13fa243f0d4362aeef9cb4f3b3a47efc5d032ff0e3afaa032ca0cb551da4', '2024-03-14 09:52:29', 0);
 
 -- --------------------------------------------------------
 
@@ -142,10 +140,7 @@ CREATE TABLE `vendor` (
 
 INSERT INTO `vendor` (`id`, `name`, `address`, `contact`) VALUES
 (1, 'ABC distributors', 'CHhetrapati', '9888888888'),
-(2, 'Rupak Distributors', 'Basundhara', '9875421036'),
-(3, 'Rupak Distributors', 'Basundhara', '9875421036'),
-(4, 'Rupak Distributors', 'Basundhara', '9875421036'),
-(5, 'Rupak Distributors', 'Basundhara', '9875421036');
+(3, 'Rupak Distributors', 'Basundhara', '9875421036');
 
 --
 -- Indexes for dumped tables
@@ -173,7 +168,8 @@ ALTER TABLE `medicine`
 -- Indexes for table `register`
 --
 ALTER TABLE `register`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `token` (`token`);
 
 --
 -- Indexes for table `vendor`
@@ -201,7 +197,7 @@ ALTER TABLE `invoice`
 -- AUTO_INCREMENT for table `medicine`
 --
 ALTER TABLE `medicine`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
 -- AUTO_INCREMENT for table `register`
